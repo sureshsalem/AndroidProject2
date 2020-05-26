87fa49b986e0e22dc2e4ef23ba8e65f2a2570da7
sureshsalem <suresh4ueee@gmail.com>
2020-05-26 12:12:23 +0530

    fifth commit

---
+++ app/src/main/java/com/example/androidproject2/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidproject2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
